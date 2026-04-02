933d802a727a3a4fca845f28fccf7d001fadbcfc
sztodo <mihastodo@yahoo.com>
2026-04-02 23:19:09 +0300

    remove redundant import

--- device-management-ui/src/app/app.ts
+++ device-management-ui/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Toast } from './shared/components/toast/toast';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, Toast, MatIcon, MatIconModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, Toast, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
